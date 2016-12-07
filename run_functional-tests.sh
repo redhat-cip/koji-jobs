751a88bd5495cd602e22613884b6b743eb34130f
Fabien Boucher <fabien.dot.boucher@gmail.com>
2016-12-07 18:44:16 +0100

    Add a koji allinone installation
    Change-Id: I38a739fc659a5e5a49476180e1aae3312aaa8cac

---
+++ run_functional-tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+sudo yum -y install epel-release
+sudo yum -y install git ansible
+
+git clone https://github.com/kostyrevaa/ansible-koji-infra
+cd ansible-koji-infra
+
+hostname=$(hostname)
+
+cat > hosts << EOF
+[koji_db]
+$hostname ansible_connection=local
+[koji_ca]
+$hostname ansible_connection=local
+[koji_hub]
+$hostname ansible_connection=local
+[koji_web]
+$hostname ansible_connection=local
+[koji_builder]
+$hostname ansible_connection=local
+EOF
+
+sudo ansible-playbook -i hosts site.yml
+sudo -u kojiadmin koji moshimoshi
